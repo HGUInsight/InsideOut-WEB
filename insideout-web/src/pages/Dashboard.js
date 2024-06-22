f8110a1a612e49b56d581fdbbefe7f21a9f6cb20
LeeMineo <lms4882@gmail.com>
2024-06-22 11:59:03 +0900

    Create informationcard
    전체적인 지표를 확인할수있는 정보카드 메인 페이지에 추가.

--- insideout-web/src/pages/Dashboard.js
+++ insideout-web/src/pages/Dashboard.js
@@ -1,10 +1,37 @@
-// src/pages/Dashboard.js
 import React from "react";
+import InformationCard from "../components/InformationCard";
 
 function Dashboard() {
   return (
     <div className="container-fluid">
-      <h1 className="h3 mb-4 text-gray-800">Dashboard</h1>
+      {/* 페이지 제목 */}
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 className="h3 mb-0 text-gray-800">어서오세요, 관리자님!</h1>
+      </div>
+
+      {/* 카드들 */}
+      <div className="row">
+        <InformationCard
+          title="일일 체크리스트를 완료한 사람"
+          value="8명"
+          icon="fa-calendar"
+        />
+        <InformationCard
+          title="평균 진행현황"
+          value="70%"
+          icon="fa-dollar-sign"
+        />
+        <InformationCard
+          title="평균 멘탈지수 증가율"
+          value="50%"
+          icon="fa-clipboard-list"
+        />
+        <InformationCard
+          title="일자리 연계 가능 인원"
+          value="2명"
+          icon="fa-comments"
+        />
+      </div>
     </div>
   );
 }
